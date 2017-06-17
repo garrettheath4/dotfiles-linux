a9c703c5a8dc92994eb104ede63b214fe14a0f70
Garrett Heath Koller <garrettheath4@gmail.com>
2017-06-17 13:09:19 -0400

    Cleaned up configure.sh script and made it more conservative (no longer deletes existing files)

--- configure.sh
+++ configure.sh
@@ -1,40 +1,26 @@
-#!/bin/bash
-# Script to configure a Mac account with preferred settings
+#!/usr/bin/env bash
+# Script to configure a Linux account with preferred settings
 # This script may also install minor programs but everything in this script
 # should run to completion without any prompts, sudo or otherwise
 
-EXPORTS="`pwd`"
+DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 HIDDENFILES='.bashrc
 .vimrc
-.gvimrc'
+.gvimrc
+.tmux.conf'
 
 HIDDENDIRS='.vim'
 
 NORMALDIRS='bin
 sbin'
 
-ConfirmOverwrite () {
-	# Usage: ConfirmOverwrite FileThatExists
-	# Returns: 0 for YES
-	# (as $?): 1 for NO
-	read -p "Warning: $1 already exists. Replace? (y/n) [n]: " confirmreplace
-	if [ "$confirmreplace" != "y" ]; then
-		# Return NO
-		echo 1
-		return 1
-	else
-		# Return YES
-		echo 0
-		return 0
-	fi
-}
-
 for hf in $HIDDENFILES; do
 	if [ -f "_$hf" ]; then
-		if [[ ! -e ~/"$hf" || "`ConfirmOverwrite ~/$hf`" == "0" ]]; then
-			rm -rf ~/"$hf"
-			ln -sfv "$EXPORTS/_$hf" ~/"$hf"
+		if [ ! -e ~/"$hf" ]; then
+			ln -sv "$DOTFILES/_$hf" ~/"$hf"
+		else
+			echo "Warning:" ~/"$hf already exists; leaving it"
 		fi
 	else
 		echo "Warning: _$hf does not exist"
@@ -43,9 +29,10 @@ done
 
 for hd in $HIDDENDIRS; do
 	if [ -d "_$hd" ]; then
-		if [[ ! -e ~/"$hd" || "`ConfirmOverwrite ~/$hd/`" == "0" ]]; then
-			rm -rf ~/"$hd"
-			ln -sfv "$EXPORTS/_$hd" ~/"$hd"
+		if [ ! -e ~/"$hd" ]; then
+			ln -sv "$DOTFILES/_$hd" ~/"$hd"
+		else
+			echo "Warning:" ~/"$hd/ already exists; leaving it"
 		fi
 	else
 		echo "Warning: _$hd does not exist"
@@ -54,11 +41,13 @@ done
 
 for nd in $NORMALDIRS; do
 	if [ -d "$nd" ]; then
-		if [[ ! -e ~/"$nd" || "`ConfirmOverwrite ~/$nd/`" == "0" ]]; then
-			rm -rf ~/"$nd"
-			ln -sfv "$EXPORTS/$nd" ~/"$nd"
+		if [ ! -e ~/"$nd" ]; then
+			ln -sv "$DOTFILES/$nd" ~/"$nd"
+		else
+			echo "Warning:" ~/"$nd/ already exists; leaving it"
 		fi
 	else
 		echo "Warning: $nd does not exist"
 	fi
 done
+
